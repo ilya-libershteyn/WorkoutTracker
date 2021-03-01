0d0afb088a8f76d60dc5db4320445a29bc36ee7c
Ilya Libershteyn <ilya.v.libershteyn@gmail.com>
2021-03-01 00:01:40 -0800

    added coded to run on heroku to server.js

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout_tracker", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
